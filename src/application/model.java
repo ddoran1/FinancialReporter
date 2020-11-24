4c320cd9cc55cbd122a926ced2c6e655d7a99c32
David Doran <davidwdoran@gmail.com>
2020-11-24 15:50:25 -0600

    Adjusted the spacing in model.java

--- src/application/model.java
+++ src/application/model.java
@@ -9,8 +9,8 @@ import javafx.collections.ObservableList;
 
 public class model {
 	private SimpleListProperty<row> rowListProperty;
-	public int temp_PK = 0; //acts as counter for the moment, but will be replaced sql command
-	
+	public int temp_PK = 0; // acts as counter for the moment, but will be replaced sql command
+
 	public model() {
 		ArrayList<row> rowList = new ArrayList<row>();
 		ObservableList<row> observableList = (ObservableList<row>) FXCollections.observableArrayList(rowList);
@@ -20,39 +20,39 @@ public class model {
 	public SimpleListProperty<row> rowListProperty() {
 		return rowListProperty;
 	}
-	
-		//INSERT OBJECT TYPE
-		public void addRow0(/*attributes to go here*/) throws ParseException {
-			row newRow = new row(/*attributes to go here*/ temp_PK);
-			temp_PK++;
-			rowListProperty.add(newRow);
-			Main.sqlConn.insertRow(/*attributes to go here*/);
-		}
 
-		//INSERT OBJECT TYPE
-		public void addRow1(/*attributes to go here*/) throws ParseException {
-			row newRow = new row(/*attributes to go here*/ temp_PK);
-			temp_PK++;
-			rowListProperty.add(newRow);
-			Main.sqlConn.insertRow(/*attributes to go here*/);
-		}
+	// INSERT OBJECT TYPE
+	public void addRow0(/* attributes to go here */) throws ParseException {
+		row newRow = new row(/* attributes to go here */ temp_PK);
+		temp_PK++;
+		rowListProperty.add(newRow);
+		Main.sqlConn.insertRow(/* attributes to go here */);
+	}
 
-		//INSERT OBJECT TYPE
-		public void addRow2(/*attributes to go here*/) throws ParseException {
-			row newRow = new row(/*attributes to go here*/ temp_PK);
-			temp_PK++;
-			rowListProperty.add(newRow);
-			Main.sqlConn.insertRow(/*attributes to go here*/);
-		}
+	// INSERT OBJECT TYPE
+	public void addRow1(/* attributes to go here */) throws ParseException {
+		row newRow = new row(/* attributes to go here */ temp_PK);
+		temp_PK++;
+		rowListProperty.add(newRow);
+		Main.sqlConn.insertRow(/* attributes to go here */);
+	}
 
-		public void deleteRow(row row) {
-			for(row r: rowListProperty) {
-				if(r.getPK() == row.getPK()) {
-					rowListProperty.remove(r);
-					//Main.model.removeItem(r); --->>> This would be for another model if needed
-					Main.sqlConn.deleteRow(row.getPK(),row.getType());
-					break;
-				}
+	// INSERT OBJECT TYPE
+	public void addRow2(/* attributes to go here */) throws ParseException {
+		row newRow = new row(/* attributes to go here */ temp_PK);
+		temp_PK++;
+		rowListProperty.add(newRow);
+		Main.sqlConn.insertRow(/* attributes to go here */);
+	}
+
+	public void deleteRow(row row) {
+		for (row r : rowListProperty) {
+			if (r.getPK() == row.getPK()) {
+				rowListProperty.remove(r);
+				// Main.model.removeItem(r); --->>> This would be for another model if needed
+				Main.sqlConn.deleteRow(row.getPK(), row.getType());
+				break;
 			}
 		}
+	}
 }
